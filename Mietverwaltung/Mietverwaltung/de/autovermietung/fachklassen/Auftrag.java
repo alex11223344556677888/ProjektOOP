5537b61b5448c11b3346825679e5913297ad5707
alex11223344556677888 <alexander.kromer11@gmail.com>
2024-06-12 20:45:52 +0200

    Update Auftrag.java

--- Mietverwaltung/Mietverwaltung/de/autovermietung/fachklassen/Auftrag.java
+++ Mietverwaltung/Mietverwaltung/de/autovermietung/fachklassen/Auftrag.java
@@ -1,5 +1,59 @@
 package de.autovermietung.fachklassen;
-//interface
-public class Auftrag  {
-   
+
+public class Auftrag {
+    private boolean kindersitz;
+    private boolean dachbox;
+    private boolean auslandsfahrt;
+    private int versicherungsklasse;
+    private boolean kilometerpaket;
+
+    public Auftrag(int id, String fzgkategorie, boolean elektrofrahrzeug, boolean parkassistent, boolean fahrassistent, boolean klimatisiert, int kundennummer, int fuehrerscheinalter, boolean kindersitz, boolean dachbox, boolean auslandsfahrt, int versicherungsklasse, boolean kilometerpaket) {
+        this.kindersitz = kindersitz;
+        this.dachbox = dachbox;
+        this.auslandsfahrt = auslandsfahrt;
+        this.versicherungsklasse = versicherungsklasse;
+        this.kilometerpaket = kilometerpaket;
+    }
+
+
+
+    public boolean isKindersitz() {
+        return kindersitz;
+    }
+
+    public void setKindersitz(boolean kindersitz) {
+        this.kindersitz = kindersitz;
+    }
+
+    public boolean isDachbox() {
+        return dachbox;
+    }
+
+    public void setDachbox(boolean dachbox) {
+        this.dachbox = dachbox;
+    }
+
+    public boolean isAuslandsfahrt() {
+        return auslandsfahrt;
+    }
+
+    public void setAuslandsfahrt(boolean auslandsfahrt) {
+        this.auslandsfahrt = auslandsfahrt;
+    }
+
+    public int getVersicherungsklasse() {
+        return versicherungsklasse;
+    }
+
+    public void setVersicherungsklasse(int versicherungsklasse) {
+        this.versicherungsklasse = versicherungsklasse;
+    }
+
+    public boolean isKilometerpaket() {
+        return kilometerpaket;
+    }
+
+    public void setKilometerpaket(boolean kilometerpaket) {
+        this.kilometerpaket = kilometerpaket;
+    }
 }
