146c3270c5b7ca41036be20156a5ff7384b8d2a8
CxlledJakob <Kuba05@web.de>
2024-07-08 20:59:04 +0200

    Update Auftrag.java

--- Mietverwaltung/Mietverwaltung/de/autovermietung/fachklassen/Auftrag.java
+++ Mietverwaltung/Mietverwaltung/de/autovermietung/fachklassen/Auftrag.java
@@ -1,11 +1,22 @@
 package de.autovermietung.fachklassen;
+import java.util.Date;
 
+import de.autovermietung.verwaltungsklassen.*;
+
+
+//interface
 public class Auftrag {
     private boolean kindersitz;
     private boolean dachbox;
     private boolean auslandsfahrt;
     private int versicherungsklasse;
     private boolean kilometerpaket;
+    private PKW pkw;
+    private Date vonDatum;
+    private Date bisDatum;
+
+    PKWVerwaltung pkwVerwaltung = new PKWVerwaltung(); 
+
 
     public Auftrag(int id, String fzgkategorie, boolean elektrofrahrzeug, boolean parkassistent, boolean fahrassistent, boolean klimatisiert, int kundennummer, int fuehrerscheinalter, boolean kindersitz, boolean dachbox, boolean auslandsfahrt, int versicherungsklasse, boolean kilometerpaket) {
         this.kindersitz = kindersitz;
@@ -13,6 +24,55 @@ public class Auftrag {
         this.auslandsfahrt = auslandsfahrt;
         this.versicherungsklasse = versicherungsklasse;
         this.kilometerpaket = kilometerpaket;
+       
+    }
+    
+
+
+    public PKW getPkw() {
+        return pkw;
+    }
+
+
+
+    public void setPkw(PKW pkw) {
+        this.pkw = pkw;
+    }
+
+
+
+    public Date getVonDatum() {
+        return vonDatum;
+    }
+
+
+
+    public void setVonDatum(Date vonDatum) {
+        this.vonDatum = vonDatum;
+    }
+
+
+
+    public Date getBisDatum() {
+        return bisDatum;
+    }
+
+
+
+    public void setBisDatum(Date bisDatum) {
+        this.bisDatum = bisDatum;
+    }
+
+
+
+    public PKWVerwaltung getPkwVerwaltung() {
+        return pkwVerwaltung;
+    }
+
+
+
+    public void setPkwVerwaltung(PKWVerwaltung pkwVerwaltung) {
+        this.pkwVerwaltung = pkwVerwaltung;
     }
 
 
