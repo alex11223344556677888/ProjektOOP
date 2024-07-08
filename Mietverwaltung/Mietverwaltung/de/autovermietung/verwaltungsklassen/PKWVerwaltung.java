15a71dc283528b58d6066c49cb90767df5595f04
CxlledJakob <Kuba05@web.de>
2024-07-08 20:57:06 +0200

    Update PKWVerwaltung.java

--- Mietverwaltung/Mietverwaltung/de/autovermietung/verwaltungsklassen/PKWVerwaltung.java
+++ Mietverwaltung/Mietverwaltung/de/autovermietung/verwaltungsklassen/PKWVerwaltung.java
@@ -10,6 +10,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 
@@ -19,12 +20,14 @@ public class PKWVerwaltung implements Serializable{
 
     public PKWVerwaltung() {
         this.pkwListe = new ArrayList<>();
-
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("pkwListe.ser"))) {
-            out.writeObject(new ArrayList<>());
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("pkwListe.ser"))) {
+            pkwListe = (List<PKW>) ois.readObject();
         } catch (IOException e) {
-            e.printStackTrace();
+            // File does not exist, create a new list
+        } catch (ClassNotFoundException e) {
+            System.err.println("Fehler beim Laden der PKW-Liste: " + e.getMessage());
         }
+        
     }
     //PKW hinzufügen
     // public void pkwHinzufuegen(String fzgkategorie, String fahrzeugmarke, String getriebe, int motorleistung, String farbe, String ausstattung, String kraftstoff, boolean klimatisiert, boolean beheizt, int baujahr, int anzahltueren, int sitzplaetze, int co2emission, int minalter, String fuehrerscheinklasse, int fzgnummer, String kennzeichen, boolean gebucht, boolean navi) {
@@ -38,21 +41,29 @@ public class PKWVerwaltung implements Serializable{
     //     }
         
     // }
+    public List<PKW> getPkwListe() {
+        return this.pkwListe;
+    }
 
-    public void pkwHinzufuegen(String fzgkategorie, String fahrzeugmarke, String getriebe, int motorleistung, String farbe, String ausstattung, String kraftstoff, boolean klimatisiert, boolean beheizt, int baujahr, int anzahltueren, int sitzplaetze, int co2emission, int minalter, String fuehrerscheinklasse, int fzgnummer, String kennzeichen, boolean gebucht, boolean navi, boolean elektrofahrzeug, boolean fahrassistent, boolean parkassistent) {
+    public void pkwHinzufuegen(String fzgkategorie, String fzgmarke, String getriebe, int motorleistung, String farbe, String ausstattung, String kraftstoff, boolean klimatisiert, boolean beheizt, int baujahr, int anzahltüren, int sitzplätze, int co2emission, int minalter, String führerscheinklasse, int fzgnummer, String kennzeichen, boolean gebucht, boolean navi, boolean elektrofahrzeug, boolean fahrassistent, boolean parkassistent) {
         int id = pkwIDCounter++;
-        PKW neuerPkw = new PKW(id, fzgkategorie, fahrzeugmarke, getriebe, motorleistung, farbe, ausstattung, kraftstoff, klimatisiert, beheizt, baujahr, anzahltueren, sitzplaetze, co2emission, minalter, fuehrerscheinklasse, fzgnummer, kennzeichen, gebucht, navi, elektrofahrzeug, fahrassistent, parkassistent);
+        PKW neuerPkw = new PKW(id, fzgkategorie, fzgmarke, getriebe, motorleistung, farbe, ausstattung, kraftstoff, klimatisiert, beheizt, baujahr, anzahltüren, sitzplätze, co2emission, minalter, führerscheinklasse, fzgnummer, kennzeichen, gebucht, navi, elektrofahrzeug, fahrassistent, parkassistent);
         if (!pkwListe.contains(neuerPkw)) {
             pkwListe.add(neuerPkw);
-            try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("pkwListe.ser"))) {
-                out.writeObject(pkwListe);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            System.out.println("PKW hinzugefügt: " + neuerPkw.getFahrzeugmarke() + " " + neuerPkw.getFzgkategorie());
+            System.out.println("PKW hinzugefügt: " + neuerPkw.getFzgmarke() + " " + neuerPkw.getFzgkategorie());
         } else {
             System.out.println("PKW bereits in der Liste vorhanden.");
         }
+    }
+
+
+   
+    public void pkwSpeichern() {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("pkwListe.ser"))) {
+        out.writeObject(pkwListe);
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
     }
 
 
@@ -62,7 +73,7 @@ public class PKWVerwaltung implements Serializable{
             System.out.println("Inhalt der PKW-Liste:");
             for (PKW pkw : pkwListe) {
                 System.out.println("ID: " + pkw.getId());
-                System.out.println("Fahrzeugmarke: " + pkw.getFahrzeugmarke());
+                System.out.println("Fahrzeugmarke: " + pkw.getFzgmarke());
                 System.out.println("Fahrzeugkategorie: " + pkw.getFzgkategorie());
                 System.out.println();
                 // man könnte noch mehr eigenschaften ausgeben aber lassen erstmal so damits übersichtlicher ist
@@ -95,9 +106,7 @@ public class PKWVerwaltung implements Serializable{
 
 
 
-    public List<PKW> getPkwListe() {
-        return this.pkwListe;
-    }
+    
     //Allgemeine Liste mit allen Eigenschaften ausgeben
     public void pkwListeAusgeben() {
         System.out.println("PKW-Liste:");
@@ -109,87 +118,143 @@ public class PKWVerwaltung implements Serializable{
     public void pkwListeAusgebenKurz() {
         System.out.println("PKW-Liste (kurz):");
         for (PKW pkw : this.pkwListe) {
-            System.out.println( pkw.getFahrzeugmarke() + " " + pkw.getFzgkategorie() + " " + pkw.getFarbe());
+            System.out.println( pkw.getFzgmarke() + " " + pkw.getFzgkategorie() + " " + pkw.getFarbe());
         }
     }
     //Liste ausgeben in Standardreihenfolge und in kurz mit Liste als Übergabeparameter
     public void pkwListeAusgebenKurz(List<PKW> pkwListe) {
         for (PKW pkw : pkwListe) {
-            System.out.println("Marke: " + pkw.getFahrzeugmarke() + ", Kategorie: " + pkw.getFzgkategorie() + " Kraftstoff: " + pkw.getKraftstoff());
+            System.out.println("Marke: " + pkw.getFzgmarke() + ", Kategorie: " + pkw.getFzgkategorie() + " Kraftstoff: " + pkw.getKraftstoff());
         }
     }
 
     //Sortierte Liste nach ID
     public void sortierePKWListeNachID() {
+        if (this.pkwListe.isEmpty()) {
+            System.out.println("Die Liste ist leer");
+            return;
+        }
     Collections.sort(this.pkwListe, Comparator.comparing(PKW::getId));
     System.out.println("PKW-Liste nach ID sortiert:");
     for (PKW pkw : this.pkwListe) {
-        System.out.println(pkw.getId() + " " + pkw.getFahrzeugmarke() + " " + pkw.getFzgkategorie());
+        System.out.println(pkw.getId() + " " + pkw.getFzgmarke() + " " + pkw.getFzgkategorie());
         }
         System.out.println();
     }
 
     //Sortierte Liste nach ID mit Liste als Übergabeparameter
     public void sortierePKWListeNachID(List<PKW> pkwListe) {
+        if (this.pkwListe.isEmpty()) {
+            System.out.println("Die Liste ist leer");
+            return;
+        }
         Collections.sort(pkwListe, Comparator.comparing(PKW::getId));
         System.out.println("PKW-Liste nach ID sortiert:");
         for (PKW pkw : pkwListe) {
-            System.out.println(pkw.getId() + " " + pkw.getFahrzeugmarke() + " " + pkw.getFzgkategorie());
+            System.out.println(pkw.getId() + " " + pkw.getFzgmarke() + " " + pkw.getFzgkategorie());
         }
         System.out.println();
     }
 
     //Sortierte Liste nach Motorisierung
     public void sortierePKWListeNachMotorisierung() {
+        if (this.pkwListe.isEmpty()) {
+            System.out.println("Die Liste ist leer");
+            return;
+        }
         Collections.sort(this.pkwListe, Comparator.comparing(PKW::getMotorleistung));
         System.out.println("PKW-Liste nach Motorisierung sortiert:");
         for (PKW pkw : this.pkwListe) {
-            System.out.println( pkw.getFahrzeugmarke() + " " + pkw.getFzgkategorie() + " " + pkw.getMotorleistung()+ "ps" );
+            System.out.println( pkw.getFzgmarke() + " " + pkw.getFzgkategorie() + " " + pkw.getMotorleistung()+ "ps" );
         }
         System.out.println();
     }
 
     //Sortierte Liste nach Motorisierung mit Liste als Übergabeparameter
     public void sortierePKWListeNachMotorisierung(List<PKW> pkwListe) {
+        if (this.pkwListe.isEmpty()) {
+            System.out.println("Die Liste ist leer");
+            return;
+        }
         Collections.sort(pkwListe, Comparator.comparing(PKW::getMotorleistung));
         System.out.println("PKW-Liste nach Motorisierung sortiert:");
         for (PKW pkw : pkwListe) {
-            System.out.println(pkw.getId() + " " + pkw.getFahrzeugmarke() + " " + pkw.getFzgkategorie() + " "+ pkw.getMotorleistung() + "ps");
+            System.out.println(pkw.getId() + " " + pkw.getFzgmarke() + " " + pkw.getFzgkategorie() + " "+ pkw.getMotorleistung() + "ps");
         }
         System.out.println();
     }
 
     //Sortierte Liste nach Baujahr
     public void sortierePKWListeNachBaujahr() {
+        if (this.pkwListe.isEmpty()) {
+            System.out.println("Die Liste ist leer");
+            return;
+        }
         Collections.sort(this.pkwListe, Comparator.comparing(PKW::getBaujahr));
         System.out.println("PKW-Liste nach Baujahr sortiert:");
         for (PKW pkw : this.pkwListe) {
-            System.out.println(pkw.getFahrzeugmarke() + " " + pkw.getFzgkategorie() + " " + pkw.getBaujahr());
+            System.out.println(pkw.getFzgmarke() + " " + pkw.getFzgkategorie() + " " + pkw.getBaujahr());
         }
         System.out.println();
     }
 
     //Sortierte Liste nach Baujahr mit Liste als Übergabeparameter
     public void sortierePKWListeNachBaujahr(List<PKW> pkwListe) {
+        if (this.pkwListe.isEmpty()) {
+            System.out.println("Die Liste ist leer");
+            return;
+        }
         Collections.sort(pkwListe, Comparator.comparing(PKW::getBaujahr));
         System.out.println("PKW-Liste nach Baujahr sortiert:");
         for (PKW pkw : pkwListe) {
-            System.out.println( pkw.getFahrzeugmarke() + " " + pkw.getFzgkategorie() + " " + pkw.getBaujahr());
+            System.out.println( pkw.getFzgmarke() + " " + pkw.getFzgkategorie() + " " + pkw.getBaujahr());
         }
         System.out.println();
     }
 
-    //PKW Status auf gebucht setzen
-    public void buchePKW(int id) {
+ 
+    
+
+//PKW Status auf gebucht setzen
+public void buchePKW(int id) {
+    try {
         for (PKW pkw : pkwListe) {
             if (pkw.getId() == id) {
+                if (pkw.isGebucht()) {
+                    throw new RuntimeException("Fahrzeug ist bereits gebucht.");
+                }
                 pkw.setGebucht(true);
+                pkwSpeichern();
                 System.out.println("PKW mit ID " + id + " wurde gebucht.");
                 return;
             }
         }
-        System.out.println("PKW mit ID " + id + " nicht gefunden.");
+        throw new RuntimeException("PKW mit ID " + id + " nicht gefunden.");
+    } catch (RuntimeException e) {
+        System.err.println(e.getMessage());
     }
+}
+
+//PKW Status auf entbucht setzen
+public void entbuchePKW(int id) {
+    try {
+        for (PKW pkw : pkwListe) {
+            if (pkw.getId() == id) {
+                if (!pkw.isGebucht()) {
+                    throw new RuntimeException("Fahrzeug ist bereits ungebucht.");
+                }
+                pkw.setGebucht(false);
+                pkwSpeichern();
+                System.out.println("PKW mit ID " + id + " wurde entbucht.");
+                return;
+            }
+        }
+        throw new RuntimeException("PKW mit ID " + id + " nicht gefunden.");
+    } catch (RuntimeException e) {
+        System.err.println(e.getMessage());
+    }
+}
+    
 
     //PKW nach ID ausgeben
     public void ausgebenPKW(int id) {
@@ -204,6 +269,9 @@ public class PKWVerwaltung implements Serializable{
 
     //PKWs filtern
     public List<PKW> filterPKW(String attribut, String wert) {
+        if (pkwListe.isEmpty()) {
+            throw new RuntimeException("Die Liste ist leer.");
+        }
         List<PKW> gefilterteListe = new ArrayList<>();
         for (PKW pkw : pkwListe) {
             switch (attribut) {
@@ -211,45 +279,105 @@ public class PKWVerwaltung implements Serializable{
                 
                     if (pkw.getGetriebe().equals(wert)) {
                         gefilterteListe.add(pkw);         
-                        System.out.println("Marke: " + pkw.getFahrzeugmarke() + ", Kategorie: " + pkw.getFzgkategorie() + " Getriebe: " + pkw.getGetriebe());            
+                        System.out.println("Marke: " + pkw.getFzgmarke() + ", Kategorie: " + pkw.getFzgkategorie() + " Getriebe: " + pkw.getGetriebe());            
                     }
                     break;
                 case "marke":
                 
-                    if (pkw.getFahrzeugmarke().equals(wert)) {
+                    if (pkw.getFzgmarke().equals(wert)) {
                         gefilterteListe.add(pkw);                   
-                        System.out.println("Marke: " + pkw.getFahrzeugmarke() + ", Kategorie: " + pkw.getFzgkategorie());                       
+                        System.out.println("Marke: " + pkw.getFzgmarke() + ", Kategorie: " + pkw.getFzgkategorie());                       
                     }
                     break;
                 case "fahrzeugtyp":
                 
                     if (pkw.getFzgkategorie().equals(wert)) {
                         gefilterteListe.add(pkw);                       
-                        System.out.println("Marke: " + pkw.getFahrzeugmarke() + ", Kategorie: " + pkw.getFzgkategorie());                       
+                        System.out.println("Marke: " + pkw.getFzgmarke() + ", Kategorie: " + pkw.getFzgkategorie());                       
                     }
                     break;
                 case "farbe":
 
                     if (pkw.getFarbe().equals(wert)) {
                         gefilterteListe.add(pkw);                      
-                        System.out.println("Marke: " + pkw.getFahrzeugmarke() + ", Kategorie: " + pkw.getFzgkategorie() + ", Farbe: " + pkw.getFarbe());
+                        System.out.println("Marke: " + pkw.getFzgmarke() + ", Kategorie: " + pkw.getFzgkategorie() + ", Farbe: " + pkw.getFarbe());
                     }
                     break;
-                    
+                case "elektro":
+                    boolean elektroWert = Boolean.parseBoolean(wert);
+                    if (pkw.isElektrofahrzeug() == elektroWert) {
+                        gefilterteListe.add(pkw);                      
+                        System.out.println("Marke: " + pkw.getFzgmarke() + ", Kategorie: " + pkw.getFzgkategorie() + ", Elektrofahrzeug: " + pkw.isElektrofahrzeug());
+                    }
+                    break;
+                case "":
+                    if (pkw.getFzgmarke().equals(wert)) {
+                        gefilterteListe.add(pkw);
+                    }
+                    break;
+                case "gebucht":
+                    boolean istGebucht = Boolean.parseBoolean(wert);
+                    if (pkw.isGebucht() == istGebucht) {
+                        gefilterteListe.remove(pkw);                      
+                    }
+                    break;
                 // Weitere Attribute hinzufügen, wenn nötig
                 default:
                     System.out.println("Attribut nicht gefunden");
                     break;
             }
         }
-
-        
         return gefilterteListe;
     }
-
-   
-
+    public List<PKW> filterPKW(String attribut, String wert, List<PKW> tempList) {
+        List<PKW> gefilterteListe = new ArrayList<>();
+        for (PKW pkw : tempList) {
+            switch (attribut) {
+                case "antrieb":
+                    if (pkw.getGetriebe().equals(wert)) {
+                        gefilterteListe.add(pkw);
+                    }
+                    break;
+                case "marke":
+                    if (pkw.getFzgmarke().equals(wert)) {
+                        gefilterteListe.add(pkw);
+                    }
+                    break;
+                case "fahrzeugtyp":
+                    if (pkw.getFzgkategorie().equals(wert)) {
+                        gefilterteListe.add(pkw);
+                    }
+                    break;
+                case "farbe":
+                    if (pkw.getFarbe().equals(wert)) {
+                        gefilterteListe.add(pkw);
+                    }
+                    break;
+                case "elektrofahrzeug":
+                    if (pkw.isElektrofahrzeug() == Boolean.parseBoolean(wert)) {
+                        gefilterteListe.add(pkw);
+                    }
+                    break;
+                case "":
+                    if (pkw.getFzgmarke().equals(wert)) {
+                        gefilterteListe.add(pkw);
+                    }
+                    break;
+                case "gebucht":
+                    boolean istGebucht = Boolean.parseBoolean(wert);
+                    if (pkw.isGebucht() == istGebucht) {
+                        gefilterteListe.remove(pkw);                      
+                    }
+                    break;
+                default:
+                    System.out.println("Attribut nicht gefunden");
+                    break;
+            }
+        }
+        return gefilterteListe;
+    }
     
+
 }
 
 
