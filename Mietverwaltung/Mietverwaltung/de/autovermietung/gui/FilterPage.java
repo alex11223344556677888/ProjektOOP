0b1cbdcc8b5274f737ceeacb1939cb6a263de08a
alex11223344556677888 <alexander.kromer11@gmail.com>
2024-07-11 23:29:39 +0200

    Update FilterPage.java

--- Mietverwaltung/Mietverwaltung/de/autovermietung/gui/FilterPage.java
+++ Mietverwaltung/Mietverwaltung/de/autovermietung/gui/FilterPage.java
@@ -25,38 +25,45 @@ public class FilterPage extends JPanel {
     private PKWVerwaltung pkwVerwaltung = new PKWVerwaltung();
 
     public FilterPage() {
-        mainPanel = new JPanel(new GridBagLayout());
+        mainPanel = new BackgroundPanel("bilder/DFF4179E-6663-4C59-9991-ACE68B2C9392.jpeg"); // Update with the correct path to your image
+        mainPanel.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(5, 5, 5, 5);
         gbc.anchor = GridBagConstraints.WEST;
 
         // Left side components
-        JLabel lblMarke = new JLabel("Marke:");
-        cmbMarke = new JComboBox<>(new String[]{"","BMW", "Audi", "Mercedes", "Opel", "VW", "Porsche"});
+        JLabel lblMarke = new RoundedLabel(" Marke: ");
+        cmbMarke = new JComboBox<>(new String[]{"", "BMW", "Audi", "Mercedes", "Opel", "VW", "Porsche"});
         gbc.gridx = 0;
         gbc.gridy = 0;
+        lblMarke.setBackground(Color.WHITE);
+        lblMarke.setForeground(Color.BLACK);
         mainPanel.add(lblMarke, gbc);
         gbc.gridx = 1;
         mainPanel.add(cmbMarke, gbc);
 
-        JLabel lblKategorie = new JLabel("Kategorie:");
-        cmbKategorie = new JComboBox<>(new String[]{"","Kleinwagen", "Kombi", "Limousine", "SUV", "Coupe", "Cabrio"});
+        JLabel lblKategorie = new RoundedLabel(" Kategorie: ");
+        cmbKategorie = new JComboBox<>(new String[]{"", "Kleinwagen", "Kombi", "Limousine", "SUV", "Coupe", "Cabrio"});
         gbc.gridx = 0;
         gbc.gridy = 1;
+        lblKategorie.setBackground(Color.WHITE);
+        lblKategorie.setForeground(Color.BLACK);
         mainPanel.add(lblKategorie, gbc);
         gbc.gridx = 1;
         mainPanel.add(cmbKategorie, gbc);
 
-        JLabel lblGetriebe = new JLabel("Getriebe:");
-        //cmbGetriebe = new JComboBox<>(new String[]{"","Manuell", "Automatik"});                   Das wieder entkommentieren, sobald die Testobjekte zw Manuell und Automatik unterscheiden nicht SCjaltgetriebe
-        cmbGetriebe = new JComboBox<>(new String[]{"","Schaltgetriebe", "Automatik"});
+        JLabel lblGetriebe = new RoundedLabel(" Getriebe: ");
+        //cmbGetriebe = new JComboBox<>(new String[]{"", "Manuell", "Automatik"}); // Das wieder entkommentieren, sobald die Testobjekte zw Manuell und Automatik unterscheiden
+        cmbGetriebe = new JComboBox<>(new String[]{"", "Schaltgetriebe", "Automatik"});
         gbc.gridx = 0;
         gbc.gridy = 2;
+        lblGetriebe.setBackground(Color.WHITE);
+        lblGetriebe.setForeground(Color.BLACK);
         mainPanel.add(lblGetriebe, gbc);
         gbc.gridx = 1;
         mainPanel.add(cmbGetriebe, gbc);
 
-        JLabel lblElektro = new JLabel("Elektro:");
+        JLabel lblElektro = new RoundedLabel(" Elektro: ");
         rdbJa = new JRadioButton("ja");
         rdbNein = new JRadioButton("nein");
         ButtonGroup bgElektro = new ButtonGroup();
@@ -64,21 +71,24 @@ public class FilterPage extends JPanel {
         bgElektro.add(rdbNein);
         gbc.gridx = 0;
         gbc.gridy = 3;
+        lblElektro.setBackground(Color.WHITE);
+        lblElektro.setForeground(Color.BLACK);
         mainPanel.add(lblElektro, gbc);
-        //mainPanel.add(bgElektro,gbc);  
-     // Ensure the buttons are directly next to each other
+
+        // Ensure the buttons are directly next to each other
         JPanel radioPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 0));
         radioPanel.add(rdbJa);
         radioPanel.add(rdbNein);
         gbc.gridx = 1;
         gbc.gridy = 3;
         mainPanel.add(radioPanel, gbc);
-       
 
-        JLabel lblFarbe = new JLabel("Farbe:");
-        cmbFarbe = new JComboBox<>(new String[]{"","Schwarz", "Weiß", "Silber", "Blau", "Rot"});
+        JLabel lblFarbe = new RoundedLabel(" Farbe: ");
+        cmbFarbe = new JComboBox<>(new String[]{"", "Schwarz", "Weiß", "Silber", "Blau", "Rot"});
         gbc.gridx = 0;
         gbc.gridy = 4;
+        lblFarbe.setBackground(Color.WHITE);
+        lblFarbe.setForeground(Color.BLACK);
         mainPanel.add(lblFarbe, gbc);
         gbc.gridx = 1;
         mainPanel.add(cmbFarbe, gbc);
@@ -90,24 +100,18 @@ public class FilterPage extends JPanel {
         mainPanel.add(btnZurueck, gbc);
         gbc.gridx = 1;
         mainPanel.add(btnSpeichern, gbc);
-        
-     // Add action listener for the Speichern button
+
+        // Add action listener for the Speichern button
         btnSpeichern.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            	//btnSpeichern.addActionListener(e -> {
                 filterPKWs();
-                    // checkMarke();
-                    // filterByMarke();
-                    // filterByKategorie();
-                    // filterByGetriebe();
-            	//});
-        };
+            }
         });
 
         // Right side components
         JLabel lblSortierenNach = new JLabel("Sortieren nach:");
-        cmbSortierenNach = new JComboBox<>(new String[]{"Preis", "Baujahr" , "Motorisierung"});
+        cmbSortierenNach = new JComboBox<>(new String[]{"Preis", "Baujahr", "Motorisierung"});
         gbc.gridx = 3;
         gbc.gridy = 0;
         mainPanel.add(lblSortierenNach, gbc);
@@ -116,13 +120,6 @@ public class FilterPage extends JPanel {
 
         listModel = new DefaultListModel<>();
         list = new JList<>(listModel);
-        // listModel.addElement("Auto 1");
-        // listModel.addElement("Auto 2");
-        // listModel.addElement("<html>Marke: Audi<br/>Preis: 123<br/>Baujahr: 2014</html>");
-        // listModel.addElement("<html>Marke: BMW<br/>Preis: 150<br/>Baujahr: 2015</html>");
-        // listModel.addElement("<html>Marke: Mercedes<br/>Preis: 200<br/>Baujahr: 2016</html>");
-        
-    
         JScrollPane scrollPane = new JScrollPane(list);
         scrollPane.setPreferredSize(new Dimension(200, 300));
         gbc.gridx = 3;
@@ -136,13 +133,11 @@ public class FilterPage extends JPanel {
         // Add action listener for list selection
         list.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent evt) {
-                @SuppressWarnings("unchecked")
-                JList<String> list = (JList<String>) evt.getSource();
                 if (evt.getClickCount() == 2) {
                     // Double-click detected
                     int index = list.locationToIndex(evt.getPoint());
-                    String item = listModel.getElementAt(index);
-                    JOptionPane.showMessageDialog(mainPanel, "Selected: " + item);
+                    PKW selectedPKW = pkwVerwaltung.getPkwListe().get(index);
+                    showCustomMessageDialog(selectedPKW);
                 }
             }
         });
@@ -155,26 +150,174 @@ public class FilterPage extends JPanel {
             parentFrame.revalidate();
             parentFrame.repaint();
         });
-
-        
-        
     }
 
     public JPanel getMainPanel() {
         return mainPanel;
     }
-    
-   
+
+    private void showCustomMessageDialog(PKW pkw) {
+        // Create a custom panel for the message dialog
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.anchor = GridBagConstraints.WEST;
+
+        JLabel lblMarke = new JLabel("Marke:");
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        panel.add(lblMarke, gbc);
+
+        JTextField txtMarke = new JTextField(pkw.getFzgmarke(), 20);
+        txtMarke.setEditable(false);
+        gbc.gridx = 1;
+        panel.add(txtMarke, gbc);
+
+        JLabel lblGetriebe = new JLabel("Getriebe:");
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        panel.add(lblGetriebe, gbc);
+
+        JTextField txtGetriebe = new JTextField(pkw.getGetriebe(), 20);
+        txtGetriebe.setEditable(false);
+        gbc.gridx = 1;
+        panel.add(txtGetriebe, gbc);
+
+        JLabel lblKraftstoff = new JLabel("Kraftstoff:");
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        panel.add(lblKraftstoff, gbc);
+
+        JTextField txtKraftstoff = new JTextField(pkw.getKraftstoff(), 20);
+        txtKraftstoff.setEditable(false);
+        gbc.gridx = 1;
+        panel.add(txtKraftstoff, gbc);
+
+        JLabel lblMotorleistung = new JLabel("Motorleistung:");
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        panel.add(lblMotorleistung, gbc);
+
+        JTextField txtMotorleistung = new JTextField(String.valueOf(pkw.getMotorleistung()), 20);
+        txtMotorleistung.setEditable(false);
+        gbc.gridx = 1;
+        panel.add(txtMotorleistung, gbc);
+
+        JLabel lblBaujahr = new JLabel("Baujahr:");
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        panel.add(lblBaujahr, gbc);
+
+        JTextField txtBaujahr = new JTextField(String.valueOf(pkw.getBaujahr()), 20);
+        txtBaujahr.setEditable(false);
+        gbc.gridx = 1;
+        panel.add(txtBaujahr, gbc);
+
+        JLabel lblFarbe = new JLabel("Farbe:");
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        panel.add(lblFarbe, gbc);
+
+        JTextField txtFarbe = new JTextField(pkw.getFarbe(), 20);
+        txtFarbe.setEditable(false);
+        gbc.gridx = 1;
+        panel.add(txtFarbe, gbc);
+
+        // Create a subpanel to hold the checkboxes and the image placeholder
+        JPanel subPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbcSub = new GridBagConstraints();
+        gbcSub.insets = new Insets(5, 5, 5, 5);
+        gbcSub.anchor = GridBagConstraints.WEST;
+
+        // Ausstattung checkboxes
+        String[] ausstattungItems1 = {"Klimaanlage", "Heizung", "Navi", "Fahrassistent", "Parkassistent"};
+        JCheckBox[] checkBoxes = new JCheckBox[ausstattungItems1.length];
+
+        for (int i = 0; i < ausstattungItems1.length; i++) {
+            checkBoxes[i] = new JCheckBox(ausstattungItems1[i]);
+            gbcSub.gridx = 0;
+            gbcSub.gridy = i;
+            subPanel.add(checkBoxes[i], gbcSub);
+        }
+
+        // Set checkbox states based on PKW attributes
+        checkBoxes[0].setSelected(pkw.isKlimatisiert());
+        checkBoxes[1].setSelected(pkw.isBeheizt());
+        checkBoxes[2].setSelected(pkw.isNavi());
+        checkBoxes[3].setSelected(pkw.isFahrassistent());
+        checkBoxes[4].setSelected(pkw.isParkassistent());
+
+        // add image
+        ImageIcon imageIcon = new ImageIcon("bilder/DFF4179E-6663-4C59-9991-ACE68B2C9392.jpeg");
+        Image image = imageIcon.getImage();
+
+        // Add the image placeholder to the right of the checkboxes
+        JPanel imagePlaceholder = new JPanel() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                if (image != null) {
+                    g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+                }
+            }
+        };
+        imagePlaceholder.setPreferredSize(new Dimension(200, 150));
+        gbcSub.gridx = 1;
+        gbcSub.gridy = 0;
+        gbcSub.gridheight = ausstattungItems1.length;
+        gbcSub.fill = GridBagConstraints.BOTH;
+        subPanel.add(imagePlaceholder, gbcSub);
+
+        gbc.gridx = 0;
+        gbc.gridy = 6;
+        gbc.gridwidth = 2;
+        panel.add(subPanel, gbc);
+
+        gbc.gridwidth = 1;
+
+        JLabel lblZeitraum = new JLabel("Zeitraum:");
+        gbc.gridx = 0;
+        gbc.gridy = 13;
+        gbc.gridwidth = 1;
+        panel.add(lblZeitraum, gbc);
+
+        JTextField txtZeitraum = new JTextField("Der vorher angegebene", 20);
+        txtZeitraum.setEditable(false);
+        gbc.gridx = 1;
+        panel.add(txtZeitraum, gbc);
+
+        JLabel lblPreis = new JLabel("Preis:");
+        gbc.gridx = 0;
+        gbc.gridy = 14;
+        panel.add(lblPreis, gbc);
+
+        JTextField txtPreis = new JTextField("100", 20);
+        txtPreis.setEditable(false);
+        gbc.gridx = 1;
+        panel.add(txtPreis, gbc);
+
+        JButton btnBuchen = new JButton("Buchen");
+        btnBuchen.setBackground(Color.ORANGE);
+        gbc.gridx = 0;
+        gbc.gridy = 15;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        panel.add(btnBuchen, gbc);
+
+        // Display the custom panel in a JOptionPane
+        JOptionPane.showMessageDialog(this, panel, "Auto Details", JOptionPane.PLAIN_MESSAGE);
+    }
 
     private void filterPKWs() {
         List<PKW> tempList = pkwVerwaltung.getPkwListe(); // start mit allen PKWs in der Liste
-        
-       // Filter für alle gebuchten PKWs
-    tempList = tempList.stream()
-            .filter(pkw -> !pkw.isGebucht())
-            .collect(Collectors.toList());
-            
-        
+
+        // Filter für alle gebuchten PKWs
+        tempList = tempList.stream()
+                .filter(pkw -> !pkw.isGebucht())
+                .collect(Collectors.toList());
+
         if (!cmbMarke.getSelectedItem().toString().isEmpty()) {
             tempList = pkwVerwaltung.filterPKW("marke", cmbMarke.getSelectedItem().toString(), tempList);
         }
@@ -192,25 +335,25 @@ public class FilterPage extends JPanel {
         }
 
         String sortBy = (String) cmbSortierenNach.getSelectedItem();
-    switch (sortBy) {
-        case "Preis":
-            pkwVerwaltung.sortierePKWListeNachID(tempList);
-            break;
-        case "Baujahr":
-            pkwVerwaltung.sortierePKWListeNachBaujahr(tempList);
-            break;
-        case "Motorisierung":
-        pkwVerwaltung.sortierePKWListeNachMotorisierung(tempList);
-            break;
-        // Add more sort options as needed
-        default:
-            break;
-    }
-    
+        switch (sortBy) {
+            case "Preis":
+                pkwVerwaltung.sortierePKWListeNachID(tempList);
+                break;
+            case "Baujahr":
+                pkwVerwaltung.sortierePKWListeNachBaujahr(tempList);
+                break;
+            case "Motorisierung":
+                pkwVerwaltung.sortierePKWListeNachMotorisierung(tempList);
+                break;
+            // Add more sort options as needed
+            default:
+                break;
+        }
+
         // Update the list model with the filtered results
         listModel.clear();
         for (PKW pkw : tempList) {
-            listModel.addElement("<html>Marke: " + pkw.getFzgmarke() + "<br/>Preis: " + pkw.getAnzahltüren() + "<br/>Baujahr: " + pkw.getBaujahr() + "<br/>------"+ "</html>");
+            listModel.addElement("<html>Marke: " + pkw.getFzgmarke() + "<br/>Preis: " + pkw.getAnzahltüren() + "<br/>Baujahr: " + pkw.getBaujahr() + "<br/>------" + "</html>");
         }
     }
 }
