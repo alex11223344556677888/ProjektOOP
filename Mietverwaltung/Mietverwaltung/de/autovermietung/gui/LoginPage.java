b89621124450fdfe30986ac0aaf780fe58547e5a
alex11223344556677888 <alexander.kromer11@gmail.com>
2024-07-11 23:34:28 +0200

    Update LoginPage.java

--- Mietverwaltung/Mietverwaltung/de/autovermietung/gui/LoginPage.java
+++ Mietverwaltung/Mietverwaltung/de/autovermietung/gui/LoginPage.java
@@ -10,27 +10,31 @@ public class LoginPage extends JPanel {
     private JPanel mainPanel;
 
     public LoginPage() {
-        mainPanel = new CustomGridPanel();
+    	mainPanel = new BackgroundPanel("bilder/DFF4179E-6663-4C59-9991-ACE68B2C9392.jpeg"); // Update with the correct path to your image
         mainPanel.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10);
         gbc.fill = GridBagConstraints.HORIZONTAL;
 
         // Header label
-        JLabel lblHeader = new JLabel("Gib deine Anmeldedaten hier bitte ein");
+        JLabel lblHeader = new RoundedLabel(" Gib deine Anmeldedaten hier bitte ein ");
         lblHeader.setFont(new Font("Arial", Font.BOLD, 24));
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.gridwidth = 2;
         gbc.anchor = GridBagConstraints.CENTER;
+        lblHeader.setBackground(Color.WHITE);
+        lblHeader.setForeground(Color.BLACK);
         mainPanel.add(lblHeader, gbc);
         
         gbc.gridwidth = 1;
         gbc.anchor = GridBagConstraints.WEST;
 
         // Email label and text field
-        JLabel lblEmail = new JLabel("E-Mail:");
+        JLabel lblEmail = new RoundedLabel(" E-Mail: ");
         JTextField txtEmail = new JTextField(15);
+        lblEmail.setBackground(Color.WHITE);
+        lblEmail.setForeground(Color.BLACK);
 
         gbc.gridx = 0;
         gbc.gridy = 1;
@@ -40,11 +44,13 @@ public class LoginPage extends JPanel {
         mainPanel.add(txtEmail, gbc);
 
         // Password label and text field
-        JLabel lblPassword = new JLabel("Passwort:");
+        JLabel lblPassword = new RoundedLabel(" Passwort: ");
         JPasswordField txtPassword = new JPasswordField(15);
 
         gbc.gridx = 0;
         gbc.gridy = 2;
+        lblPassword.setBackground(Color.WHITE);
+        lblPassword.setForeground(Color.BLACK);
         mainPanel.add(lblPassword, gbc);
         
         gbc.gridx = 1;
@@ -59,6 +65,7 @@ public class LoginPage extends JPanel {
         btnLogin.setPreferredSize(buttonSize);
 
         JPanel buttonPanel = new JPanel(new GridBagLayout());
+        buttonPanel.setOpaque(false);
         GridBagConstraints buttonGbc = new GridBagConstraints();
         buttonGbc.insets = new Insets(10, 10, 10, 10);
         
@@ -84,14 +91,16 @@ public class LoginPage extends JPanel {
                 new WelcomePage().setVisible(true);
             }
         });*/
+        
+        // Zurück button 
         btnBack.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JFrame parentFrame = (JFrame) SwingUtilities.getWindowAncestor(mainPanel);
-                parentFrame.dispose(); // Close the current frame
-
-                WelcomePage welcomePage = new WelcomePage();
-                welcomePage.setVisible(true); // Make the WelcomePage frame visible
+            	JFrame parentFrame = (JFrame) SwingUtilities.getWindowAncestor(mainPanel);
+                parentFrame.getContentPane().removeAll();
+                parentFrame.getContentPane().add(new WelcomePage().getMainPanel());
+                parentFrame.revalidate();
+                parentFrame.repaint();
             }
         });
      // Add action listener for the Save button
@@ -100,7 +109,7 @@ public class LoginPage extends JPanel {
             public void actionPerformed(ActionEvent e) {
                 JFrame parentFrame = (JFrame) SwingUtilities.getWindowAncestor(mainPanel);
                 parentFrame.getContentPane().removeAll();
-                parentFrame.getContentPane().add(new KalenderPage().getMainPanel());
+                parentFrame.getContentPane().add(new Auftraguebersicht().getMainPanel());
                 parentFrame.revalidate();
                 parentFrame.repaint();
             }
