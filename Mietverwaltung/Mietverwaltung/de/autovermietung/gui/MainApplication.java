acf00f4bb997008a1f64de40054264591f6322e4
alex11223344556677888 <alexander.kromer11@gmail.com>
2024-07-11 23:35:05 +0200

    Update MainApplication.java

--- Mietverwaltung/Mietverwaltung/de/autovermietung/gui/MainApplication.java
+++ Mietverwaltung/Mietverwaltung/de/autovermietung/gui/MainApplication.java
@@ -4,9 +4,22 @@ import javax.swing.*;
 
 
 public class MainApplication {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Welcome Page");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            frame.getContentPane().add(new WelcomePage().getMainPanel());
+            frame.pack();
+            frame.setVisible(true);
+        });
+    }
+}
+
+/**
+public class MainApplication {
 	    public static void main(String[] args) {
 	        SwingUtilities.invokeLater(() -> new WelcomePage());
 	    }
-	}
-
+	} */
 
