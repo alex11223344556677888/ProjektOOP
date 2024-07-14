02efa12c6e0506548d5bb7e5c9a9aba31c6d24a6
TwanBVB09 <dntchlgm@gmail.com>
2024-07-15 01:22:44 +0200

    Update MainApplication.java

--- Mietverwaltung/Mietverwaltung/de/autovermietung/gui/MainApplication.java
+++ Mietverwaltung/Mietverwaltung/de/autovermietung/gui/MainApplication.java
@@ -1,20 +1,22 @@
 package Gui;
 
 import javax.swing.*;
-
+import Verwaltungsklassen.Kundenverwaltung;
 
 public class MainApplication {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            Kundenverwaltung kundenverwaltung = new Kundenverwaltung();
             JFrame frame = new JFrame("Welcome Page");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-            frame.getContentPane().add(new WelcomePage().getMainPanel());
+            frame.getContentPane().add(new WelcomePage(kundenverwaltung).getMainPanel()); // Pass kundenverwaltung
             frame.pack();
             frame.setVisible(true);
         });
     }
 }
+
 
 /**
 public class MainApplication {
