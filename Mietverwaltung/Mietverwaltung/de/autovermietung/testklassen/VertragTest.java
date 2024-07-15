2ca554d1cb879874c8aa11502b1c7debed14591a
eric05k <eric.koellner@outlook.de>
2024-07-15 21:57:20 +0200

    VertragTest.java

--- Mietverwaltung/Mietverwaltung/de/autovermietung/testklassen/VertragTest.java
+++ Mietverwaltung/Mietverwaltung/de/autovermietung/testklassen/VertragTest.java
@@ -1,46 +1,55 @@
-package Testklassen;
+package autovermietung.Testklassen;
 
 import java.time.LocalDate;
 
-import fachklassen.Kunde;
-import fachklassen.PKW;
-import fachklassen.Vertrag;
+import autovermietung.Fachklassen.Kunde;
+import autovermietung.Fachklassen.PKW;
+import autovermietung.Fachklassen.Vertrag;
 
 
-import Verwaltungsklassen.Kundenverwaltung;
-import Verwaltungsklassen.PKWVerwaltung;
-
+import autovermietung.Verwaltungsklassen.Kundenverwaltung;
+import autovermietung.Verwaltungsklassen.PKWVerwaltung;
 
 
 public class VertragTest {
+    
+    
     public static void main(String[] args) {
-        // Objekte
+        // Objekte erstellen
         Kundenverwaltung kundenverwaltung = new Kundenverwaltung();
         PKWVerwaltung pkwverwaltung = new PKWVerwaltung();
         
         // Beispiel Kunde und PKW
-        Kunde kunde = kundenverwaltung.getKundeByKundennummer(123456789);
-        PKW pkw = pkwverwaltung.getPkwById(1000);
+        Kunde kunde1 = kundenverwaltung.getKundeByKundennummer(123456);
+        Kunde kunde2 = kundenverwaltung.getKundeByKundennummer(123456789);
+        PKW pkw1 = pkwverwaltung.getPkwById(1002);
+        PKW pkw2 = pkwverwaltung.getPkwById(1003);
 
         // Vertragsdetails definieren
         LocalDate vertragsbeginn = LocalDate.of(2023, 3, 1);
         LocalDate vertragsende = LocalDate.of(2023, 3, 10);
 
         // Vertrag erstellen
-        Vertrag.erstelleVertrag(kunde, pkw, vertragsbeginn, vertragsende);
+        Vertrag vertrag1 = new Vertrag(kunde1, pkw1, vertragsbeginn, vertragsende);
+        vertrag1.erstelleVertrag(kunde1, pkw1, vertragsbeginn, vertragsende);
+        Vertrag vertrag2 = new Vertrag(kunde2, pkw2, vertragsbeginn, vertragsende);
+        vertrag2.erstelleVertrag(kunde2, pkw2, vertragsbeginn, vertragsende);
+        
+
+        
+        
 
         // Vertrag anzeigen
-        Vertrag.vertragAnzeigen(kunde, pkw);
+        vertrag1.vertragAnzeigen(kunde1, pkw1);
+        vertrag2.vertragAnzeigen(kunde2, pkw2);
 
-        // Vertrag bearbeiten
-        vertragsbeginn = LocalDate.of(2023, 3, 5);
-        vertragsende = LocalDate.of(2023, 3, 15);
-        Vertrag.vertragBearbeiten(kunde, pkw, vertragsbeginn, vertragsende);
-
-        // Vertrag anzeigen nach Bearbeiten
-        Vertrag.vertragAnzeigen(kunde, pkw);
+    
 
         // Vertrag löschen
-        Vertrag.vertragLoeschen(kunde, pkw);
+        vertrag1.vertragLoeschen(kunde1, pkw1);
+
+        // Vertrag anzeigen
+        vertrag1.vertragAnzeigen(kunde1, pkw1);
+        vertrag2.vertragAnzeigen(kunde2, pkw2);
     }
 }
